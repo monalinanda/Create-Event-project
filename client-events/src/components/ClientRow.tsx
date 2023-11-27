12873485414e7724466f0be03fa9a905641a71e3
monalinanda <monalinanda95@gmail.com>
2023-11-27 17:11:24 +0530

    add type to props key

--- client-events/src/components/ClientRow.tsx
+++ client-events/src/components/ClientRow.tsx
@@ -4,31 +4,32 @@ import { DELETE_CLIENT } from "../mutations/clientMutations";
 import { GET_CLIENTS } from '../queries/clientQueries';
 import { GET_EVENTS } from '../queries/eventQueries';
 
-interface ClientProps {
-  client:{
-      id : string;
-      name : string;
-      email : string;
-      phone : string
-  }
+
+interface client {
+  id : string;
+  name : string;
+  email : string;
+  phone : string
+}
+interface Props {
+  id: string;
+  client: client;
 }
 
-export default function ClientRow({ client}: ClientProps) {
-  // const [deleteClient] = useMutation(DELETE_CLIENT, {
-  //    variables: { id: client.id },
-  //   refetchQueries: [{ query: GET_CLIENTS }, { query: GET_EVENTS }],
-  // });
+
+
+export default function ClientRow( Props : Props) {
   const [deleteClient] = useMutation(DELETE_CLIENT)
 
   return (
     <tr>
-      <td>{client.name}</td>
-      <td>{client.email}</td>
-      <td>{client.phone}</td>
+      <td>{Props.client.name}</td>
+      <td>{Props. client.email}</td>
+      <td>{Props.client.phone}</td>
       <td>
         {/* <button className='btn btn-danger btn-sm' onClick={deleteClient}> */}
         <button className='btn btn-danger btn-sm' onClick={()=>deleteClient( {
-     variables: { id: client.id },
+     variables: { id: Props.client.id },
     refetchQueries: [{ query: GET_CLIENTS }, { query: GET_EVENTS }]})}>
           <FaTrash />
         </button>
